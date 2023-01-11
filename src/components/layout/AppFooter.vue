9762e6c3315754679d18df770d00fba57bd13846
wxf <1019072635@qq.com>
2023-01-11 22:10:43 +0800

    feat: 添加登录页底部信息

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="footer">
+    <div class="mr-2">Copyright © 2022-2023 baldbear.cn All Rights Reserved.</div>
+    <div>
+      <a href="https://beian.miit.gov.cn/" target="_blank">备案号：冀ICP备2022030887号</a>
+    </div>
   </div>
 </template>
 
@@ -10,8 +14,23 @@ export default {
 </script>
 
 <style scoped>
-.footer{
+.footer {
   height: 80px;
-  background-color: transparent;
+  background-color: rgb(244, 245, 245);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 13px;
+  color: rgb(136, 136, 136);
 }
+a {
+  text-decoration:none;
+}
+a:link {
+  color:rgb(136, 136, 136);
+}
+a:visited {
+  color:rgb(136, 136, 136);
+}
+
 </style>
